5d1c84ed2d1ae4e9ccab537c36ce2ab0ab6a6823
Duvan Rosero <drosero@talenta.cloud>
2023-10-23 12:31:40 -0500

    refactor: change version by date get time

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private version: number = new Date().getTime();
   getHello(): object {
     return {
-      message: 'Hola mundo',
+      message: 'Hola muasdasndo',
       developer: 'Duvan Rosero',
+      version: this.version,
       error: false,
       env: process.env.NODE_ENV,
       base_url: process.env.BASE_URL,
