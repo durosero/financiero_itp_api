3c5a99d0afdb738ba4808bfe49fb2eed4ae9b350
Duvan Rosero <drosero@talenta.cloud>
2023-12-11 17:57:37 -0500

    build: change package conection db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+import { join, resolve } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import config from './config/config';
@@ -27,10 +27,14 @@ import { TasksService } from './services/tasks.service';
       username: process.env.MYSQL_SGD_USER,
       password: process.env.MYSQL_SGD_PASS,
       database: process.env.MYSQL_SGD_DATABASE,
-      entities: [],
+      entities: [
+        resolve(__dirname, 'modules/invoice/entities/*.entity{.ts,.js}'),
+      ],
       autoLoadEntities: true,
       synchronize: false,
       logging: 'all',
+      retryAttempts: 30,
+      insecureAuth:true
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, 'assets'),
