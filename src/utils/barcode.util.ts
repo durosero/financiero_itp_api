256a37a65e611f3f39e6d0c7a5a72eae4fbdd870
Duvan Rosero <drosero@talenta.cloud>
2024-05-29 09:06:16 -0500

    refactor: increase number of characters 3900 section

--- src/utils/barcode.util.ts
+++ src/utils/barcode.util.ts
@@ -18,8 +18,8 @@ export const generarCodigoBarrasString = ({
   let valor390n: string = value.toString();
   const fecha96: string = moment(limitDate).format('YYYYMMDD');
 
-  const length8020: number = 12;
-  const length390n: number = 10;
+  const length8020 = 12;
+  const length390n = 14;
 
   if (referencia8020.length > length8020) {
     throw new Error('El codigo de referencia supera el máximo permitido');
